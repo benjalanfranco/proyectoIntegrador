274bf0e6bba02252f7a113580bcbed97345a9f72
benjalanfranco <benjalanfranco@udesa.edu.ar>
2021-09-24 17:45:52 -0300

    mini cambio

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,6 @@ var router = express.Router();
 
 let indexController = require('../controllers/indexController')
 
-<<<<<<< HEAD
 router.get('/', indexController.index);
-=======
-router.get('/index', indexController.index);
->>>>>>> 5fa5f8374b0eec86cad75952811bdec324411b1f
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-let usersController = require('../controllers/usersController')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
