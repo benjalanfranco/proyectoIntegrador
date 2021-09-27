05ff7b6afba37eed96e93125686374d8e0ad4143
benjalanfranco <benjalanfranco@udesa.edu.ar>
2021-09-27 17:28:52 -0300

    agregarPost

--- routes/post.js
+++ routes/post.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 let postController = require('../controllers/postController')
 
-router.get('/:id', postController.detallePost);
 router.get('/agregarPost', postController.agregar);
+router.get('/:id', postController.detallePost);
+
 
 
 module.exports = router;
