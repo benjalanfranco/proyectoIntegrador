f1204d94e9211bf60ac6e3769d800a2dcd0a678d
benjalanfranco <benjalanfranco@udesa.edu.ar>
2021-11-08 16:15:02 -0300

    agregando funcionalidades

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postRouter = require('./routes/post');
@@ -19,6 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session( {secret: "Para identificar Deporter",
+                  resave: false,
+                  saveUninitialized: true }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
