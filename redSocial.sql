4804d2a8253d856c2f1665ea89e1af0f45ae4bc7
benjalanfranco <benjalanfranco@udesa.edu.ar>
2021-11-11 10:34:28 -0300

    controladores y modelos

--- redSocial.sql
+++ redSocial.sql
@@ -30,7 +30,7 @@ Categ varchar(20) not null
 );
 
 CREATE TABLE `Posts` (
-  `idPosts` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `idUsuario` int(10) unsigned,
   `Ubicacion` varchar(45) DEFAULT NULL,
   `Imagen` varchar(45) DEFAULT NULL,
