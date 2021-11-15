8ebe2cf01714cc2fffcc6d60d0822b2f32047195
Valen Ulloa <valenulloa@MacBook-Pro-de-Valen.local>
2021-11-15 16:34:50 -0300

    mac

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "RedSocial",
     "host": "127.0.0.1",
     "port": 8889,
