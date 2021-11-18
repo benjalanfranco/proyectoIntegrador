b7e7022dc04b0bb9afe5661b4ae3556735734209
Valen Ulloa <valenulloa@MacBook-Pro-de-Valen.local>
2021-11-18 10:51:33 -0300

    fix

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,20 @@ var router = express.Router();
 let usersController = require('../controllers/usersController')
 
 router.get('/miPerfil', usersController.miPerfil);
+
+
 router.get('/editarPerfil', usersController.editarPerfil);
+
+
 router.get('/:usuario', usersController.detalleUsuario);
+
+
 router.get('/login', usersController.login);
+
+
 router.get('/registracion', usersController.registrarse);
+
+
 
 
 module.exports = router;
