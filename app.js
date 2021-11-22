798f2cc7c4d492858b04a89708e01adfae0739b6
benjalanfranco <benjalanfranco@udesa.edu.ar>
2021-11-22 20:05:42 -0300

    buscador

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
-//const session = require('express-session');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,6 +13,10 @@ const db = require('./database/models');
 var app = express();
 
 db.sequelize.sync({alter:true});
+
+app.use(session( {secret: "Para identificar Deporter",
+                  resave: false,
+                  saveUninitialized: true }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,9 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(session( {secret: "Para identificar Deporter",
-                 // resave: false,
-                 // saveUninitialized: true }));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
